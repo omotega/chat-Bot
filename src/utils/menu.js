b7a257e738a33cc37c65a4b94864fedeb1198948
omotega <tomoyibo@gmail.com>
2023-03-16 18:41:10 +0100

    food menu

---
+++ src/utils/menu.js
@@ -0,0 +1,11 @@
+
+const menu = [
+  { number: 2, food: "sausage", price: 400 },
+  { number: 3, food: "ice-cream", price: 600 },
+  { number: 4, food: "jollof-rice and beef", price: 1700 },
+  { number: 5, food: " bottle-water", price: 800 },
+];
+
+module.exports = {
+  menu,
+};
