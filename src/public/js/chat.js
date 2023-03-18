ef5060b6552bf740b7cea22ab70c32c81b6c727e
omotega <tomoyibo@gmail.com>
2023-03-18 02:19:43 +0100

    updated my js file

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,40 +1,43 @@
 var socket = io("ws://localhost:5750");
 var messages = document.getElementById("messages");
-const chat = document.getElementById('form');
-const inputField = document.getElementById('message');
+const chat = document.getElementById("form");
+const inputField = document.getElementById("message");
 
-
-socket.on("bot-message", data => {
-  console.log(data,'this is data')
+socket.on("bot-message", (data) => {
   let li = document.createElement("li");
   let span = document.createElement("span");
   var messages = document.getElementById("messages");
-  messages.appendChild(li).append(data);
-  console.log(messages)
+  if (Array.isArray(data.text)) {
+    data.text.forEach((items) => {
+      var x = document.createElement("LI");
+      var t = document.createTextNode(items);
+      x.appendChild(t);
+      messages.appendChild(x);
+    });
+    return
+  }
+  messages.appendChild(li).append(data.text);
+  console.log(Array.isArray(data));
+  
   messages.appendChild(span).append(`By ${data.username} at ${data.time}`);
 });
 
-
-chat.addEventListener('submit', (e) => {
+chat.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let Msg = e.target.elements.inputMessage.value;
+  let Msg = e.target.elements.message.value;
 
   let message = Msg.trim();
 
   if (!message) {
-        alert('Input shouldnt be empty');
-        return;
+    alert("Input shouldn't be empty");
+    return;
   }
 
-  if (message !== '') {
-        
-        socket.emit('chat-message', message);
-      
-        e.target.elements.inputMessage.value = '';
-        e.target.elements.inputMessage.focus();
+  if (message !== "") {
+    socket.emit("chat-message", message);
+
+    e.target.elements.message.value = "";
+    e.target.elements.message.focus();
   }
 });
-
-
-
